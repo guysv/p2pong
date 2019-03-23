7ae267952de5f1130cc19d09fb6447f6fee934da
guysv <sviryguy@gmail.com>
2019-03-23 19:39:01 +0200

    Tide up vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
-var path = require('path')
-
 module.exports = {
   publicPath: './',
   configureWebpack: config => {
-    config.resolve.alias = {
-      ipfs: 'ipfs/dist/index.min.js',
-      '@': path.resolve('src')
-    }
+    config.resolve.alias.ipfs = 'ipfs/dist/index.min.js'
   }
 }
